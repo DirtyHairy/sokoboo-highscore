ed391dbedfecf249bc66aaadca60d302408e25a3
Christian Speckner <christian.speckner@mayflower.de>
2019-09-30 21:09:31 +0200

    Add asset versioning for cache busting.

---
+++ src/Assets/BuildIdVersionStrategy.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Assets;
+
+
+use Symfony\Component\Asset\VersionStrategy\VersionStrategyInterface;
+
+class BuildIdVersionStrategy implements VersionStrategyInterface
+{
+    private const BUILD_ID_KEY = "BUILD_ID";
+
+    /**
+     * Returns the asset version for an asset.
+     *
+     * @param string $path A path
+     *
+     * @return string The version string
+     */
+    public function getVersion($path): string
+    {
+        return array_key_exists(self::BUILD_ID_KEY, $_ENV) ? $_ENV[self::BUILD_ID_KEY] : "";
+    }
+
+    /**
+     * Applies version to the supplied path.
+     *
+     * @param string $path A path
+     *
+     * @return string The versionized path
+     */
+    public function applyVersion($path): string
+    {
+        $version = $this->getVersion($path);
+
+        return empty($version) ? $path : sprintf("%s?v=%s", $path, $this->getVersion($path));
+    }
+}
