22c241ee0dd3ee935e6915d91f0fa12d0791f828
Christian Speckner <christian.speckner@mayflower.de>
2019-10-07 01:26:39 +0200

    Cleanup styling.

--- assets/ts/matrix/Tooltip.tsx
+++ assets/ts/matrix/Tooltip.tsx
@@ -3,7 +3,6 @@
 import { FunctionComponent } from 'react';
 
 import { jsx } from '@emotion/core';
-import styled from '@emotion/styled';
 
 import LevelStatistics from '../model/LevelStatistics';
 import { WIDE } from './definitions';
@@ -13,31 +12,18 @@ export interface Props {
     statistics: LevelStatistics;
 }
 
-const PlayerIcon: FunctionComponent = () => (
-    <i
-        css={{
-            width: '1rem',
-            height: '1rem',
-            backgroundSize: 'contain',
-            display: 'inline-block',
-            verticalAlign: 'bottom'
-        }}
-        className="player-icon"
-    />
-);
-
 const TRANSFORM = 'translateY(-100%) translate(-5px, 5px)';
 
 const Tooltip: FunctionComponent<Props> = props => (
     <div
         css={{
             position: 'absolute',
-            width: '7rem',
+            width: '7em',
             backgroundColor: 'black',
             color: 'white',
             boxShadow: '0 0 32px 3px #333',
             borderCollapse: 'separate',
-            padding: '0.5rem',
+            padding: '0.5em',
             left: WIDE,
             zIndex: 10,
             transform: TRANSFORM,
@@ -47,11 +33,11 @@ const Tooltip: FunctionComponent<Props> = props => (
         }}
         className="tooltip"
     >
-        <div css={{ marginBottom: '0.5rem' }}>
+        <div css={{ marginBottom: '0.5em' }}>
             ☰ {props.level}/${props.level.toString(16).padStart(2, '0')}
         </div>
-        <div css={{ marginBottom: '0.5rem' }}>
-            <PlayerIcon /> {props.statistics.playedCount}
+        <div css={{ marginBottom: '0.5em' }}>
+            <i className="player-icon" /> {props.statistics.playedCount}
         </div>
         <div># {props.statistics.bestScore}</div>
     </div>
