6c24ff4b80268c6a78aca24a64762d43daf801ad
Christian Speckner <christian.speckner@mayflower.de>
2019-11-17 23:07:07 +0100

    Fix session lifetime.

---
+++ src/Migrations/Version20191117220010.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191117220010 extends AbstractMigration
+{
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    /**
+     * @param Schema $schema
+     * @throws DBALException
+     */
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("
+            ALTER TABLE `session` MODIFY COLUMN`sess_lifetime` INTEGER NOT NULL;
+        ");
+    }
+
+    /**
+     * @param Schema $schema
+     * @throws DBALException
+     */
+    public function down(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("
+            ALTER TABLE `session` MODIFY COLUMN`sess_lifetime` MEDIUMINT NOT NULL;
+        ");
+    }
+}
